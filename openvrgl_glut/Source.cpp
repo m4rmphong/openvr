4142eb2360c5cf6cc7888bc9e4fa1b8d26ac9920
m4rmphong <m4rmphong.cs01@g2.nctu.edu.tw>
2017-10-20 14:44:11 +0800

    openvr setup but unable to render anything

--- openvrgl_glut/Source.cpp
+++ openvrgl_glut/Source.cpp
@@ -13,10 +13,36 @@
 #include <glm/mat4x4.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-#include "OpenVRGL.h"
+//#include "OpenVRGL.h"
 
 using namespace std;
 using namespace cv;
+
+//-------------------------------------------------------------------------------
+// Struct
+//-------------------------------------------------------------------------------
+
+struct VertexDataScene {
+	glm::vec3 position;
+	glm::vec2 texCoord;
+	VertexDataScene(const glm::vec3& pos, const glm::vec2& tex) : position(pos), texCoord(tex) {}
+};
+
+struct VertexDataWindow { // companion window
+	glm::vec2 position;
+	glm::vec2 texCoord;
+};
+
+struct FramebufferDesc {
+	GLuint m_nDepthBufferId;
+	GLuint m_nRenderTextureId;
+	GLuint m_nRenderFramebufferId;
+	GLuint m_nResolveTextureId;
+	GLuint m_nResolveFramebufferId;
+};
+FramebufferDesc leftEyeDesc;
+FramebufferDesc rightEyeDesc;
+
 
 //-------------------------------------------------------------------------------
 // Variables
@@ -28,7 +54,30 @@ std::thread		g_threadLoadFrame;
 bool			g_bUpdated = false;
 bool			g_bRunning = true;
 
-COpenVRGL* g_pOpenVRGL = nullptr;
+// openvr variable
+//COpenVRGL* g_pOpenVRGL = nullptr;
+vr::IVRSystem *m_pHMD;
+vr::IVRRenderModels *m_pRenderModels;
+vr::TrackedDevicePose_t m_rTrackedDevicePose[vr::k_unMaxTrackedDeviceCount];
+glm::mat4x4 m_rmat4DevicePose[vr::k_unMaxTrackedDeviceCount];
+
+float m_fNearClip;
+float m_fFarClip;
+
+uint32_t m_nRenderWidth;
+uint32_t m_nRenderHeight;
+
+// pose matrix
+glm::mat4 m_mat4HMDPose;
+glm::mat4 m_mat4eyePosLeft;
+glm::mat4 m_mat4eyePosRight;
+
+glm::mat4 m_mat4ProjectionCenter;
+glm::mat4 m_mat4ProjectionLeft;
+glm::mat4 m_mat4ProjectionRight;
+
+glm::mat4 m_mat4VPLeft;
+glm::mat4 m_mat4VPRight;
 
 GLuint m_frameTexture;
 
@@ -36,18 +85,87 @@ int frameWindowVertCount;
 GLuint g_frameWindowVAO;
 GLuint frameWindowVertexbuffer;
 GLuint g_renderFrameWindowProgramID;
+GLint m_frameWindowMatrixLocation;
 
 //-------------------------------------------------------------------------------
 // Purpose: Functions
 //-------------------------------------------------------------------------------
+
+glm::mat4 MVPmatrix() {
+	glm::mat4 proj = glm::perspective(glm::radians(45.0f),(float)g_camFrame.cols/(float)g_camFrame.rows,0.1f,30.f);
+	glm::mat4 view = glm::lookAt(glm::vec3(2,2,1),glm::vec3(0,0,0),glm::vec3(0,1,0));
+	glm::mat4 model = glm::mat4(1.0f);
+	glm::mat4 mvp = proj*view*model;
+	return mvp;
+}
+
+glm::mat4 ConvertMat(const vr::HmdMatrix34_t& mat)
+{
+	return  glm::mat4(
+		mat.m[0][0], mat.m[1][0], mat.m[2][0], 0.0,
+		mat.m[0][1], mat.m[1][1], mat.m[2][1], 0.0,
+		mat.m[0][2], mat.m[1][2], mat.m[2][2], 0.0,
+		mat.m[0][3], mat.m[1][3], mat.m[2][3], 1.0f
+	);
+}
+
+glm::mat4 ConvertMat(const vr::HmdMatrix44_t& mat)
+{
+	return  glm::mat4(
+		mat.m[0][0], mat.m[1][0], mat.m[2][0], mat.m[3][0],
+		mat.m[0][1], mat.m[1][1], mat.m[2][1], mat.m[3][1],
+		mat.m[0][2], mat.m[1][2], mat.m[2][2], mat.m[3][2],
+		mat.m[0][3], mat.m[1][3], mat.m[2][3], mat.m[3][3]
+	);
+}
+
+bool CreateFrameBuffer(int nWidth, int nHeight, FramebufferDesc& framebufferDesc) {
+	glGenFramebuffers(1, &framebufferDesc.m_nRenderFramebufferId);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebufferDesc.m_nRenderFramebufferId);
+
+	glGenRenderbuffers(1, &framebufferDesc.m_nDepthBufferId);
+	glBindRenderbuffer(GL_RENDERBUFFER, framebufferDesc.m_nDepthBufferId);
+	glRenderbufferStorageMultisample(GL_RENDERBUFFER, 4, GL_DEPTH_COMPONENT, nWidth, nHeight);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, framebufferDesc.m_nDepthBufferId);
+
+	glGenTextures(1, &framebufferDesc.m_nRenderTextureId);
+	glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, framebufferDesc.m_nRenderTextureId);
+	glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, 4, GL_RGBA8, nWidth, nHeight, true);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, framebufferDesc.m_nRenderTextureId, 0);
+
+	glGenFramebuffers(1, &framebufferDesc.m_nResolveFramebufferId);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebufferDesc.m_nResolveFramebufferId);
+
+	glGenTextures(1, &framebufferDesc.m_nResolveTextureId);
+	glBindTexture(GL_TEXTURE_2D, framebufferDesc.m_nResolveTextureId);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, nWidth, nHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, framebufferDesc.m_nResolveTextureId, 0);
+
+	// check FBO status
+	GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	if (status != GL_FRAMEBUFFER_COMPLETE)
+	{
+		return false;
+	}
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	return true;
+}
+
+//-------------------------------------------------------------------------------
+// Purpose: Setup
+//-------------------------------------------------------------------------------
 void SetupScene() {
 	static const GLfloat g_vertex_buffer_data[] = {
-		-0.5f, -0.5f, 0.0f,  0.0f,  1.0f, // B
-		 0.5f, -0.5f, 0.0f,  1.0f,  1.0f, // A
-		 0.5f,  0.5f, 0.0f,  1.0f,  0.0f, // D
-		 0.5f,  0.5f, 0.0f,  1.0f,  0.0f, // D
-		-0.5f,  0.5f, 0.0f,  0.0f,  0.0f, // C
-		-0.5f, -0.5f, 0.0f,  0.0f,  1.0f, // B
+		-0.5f, -0.5f, -5.0f,  0.0f,  1.0f, // B
+		 0.5f, -0.5f, -5.0f,  1.0f,  1.0f, // A
+		 0.5f,  0.5f, -5.0f,  1.0f,  0.0f, // D
+		 0.5f,  0.5f, -5.0f,  1.0f,  0.0f, // D
+		-0.5f,  0.5f, -5.0f,  0.0f,  0.0f, // C
+		-0.5f, -0.5f, -5.0f,  0.0f,  1.0f, // B
 	};
 	frameWindowVertCount = 6;
 
@@ -89,6 +207,42 @@ void SetupScene() {
 	glDisableVertexAttribArray(1);
 }
 
+void SetupCamera() {
+	if (!m_pHMD) return;
+	m_mat4ProjectionLeft = ConvertMat(m_pHMD->GetProjectionMatrix(vr::Eye_Left, m_fNearClip, m_fFarClip));
+	m_mat4ProjectionRight = ConvertMat(m_pHMD->GetProjectionMatrix(vr::Eye_Right, m_fNearClip, m_fFarClip));
+	m_mat4eyePosLeft = ConvertMat(m_pHMD->GetEyeToHeadTransform(vr::Eye_Left));
+	m_mat4eyePosRight = ConvertMat(m_pHMD->GetEyeToHeadTransform(vr::Eye_Right));
+	// ViewProjection Matrix VP = Projection*View(position respect to origin)
+	// MVP = VP*m_mat4HMDPose(current hmd pose); update HMD pose only
+	m_mat4VPLeft = m_mat4ProjectionLeft*m_mat4eyePosLeft;
+	m_mat4VPRight = m_mat4ProjectionRight*m_mat4eyePosRight;
+}
+
+void SetupStereoRenderTargets() {
+	// setup frame buffer for the views of two eyes
+	// get the proper width and height
+	m_pHMD->GetRecommendedRenderTargetSize(&m_nRenderWidth, &m_nRenderHeight);
+	// use the width and height information to create frame buffer
+	CreateFrameBuffer(m_nRenderWidth, m_nRenderHeight, leftEyeDesc/*Struct to strore the framebuffer information*/); // send struct in
+	CreateFrameBuffer(m_nRenderWidth, m_nRenderHeight, rightEyeDesc/*Struct to strore the framebuffer information*/);
+}
+
+void SetupCompanionWindow() {
+
+}
+
+glm::mat4x4 GetCurrentMVP(vr::Hmd_Eye eyeIdx) {
+	glm::mat4x4 matMVP;
+	if (eyeIdx == vr::Eye_Left) {
+		matMVP = m_mat4VPLeft*m_mat4HMDPose;
+	}
+	else {
+		matMVP = m_mat4VPRight*m_mat4HMDPose;
+	}
+	return matMVP;
+}
+
 void RenderScene() {
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	// 1rst attribute buffer : vertices
@@ -103,20 +257,88 @@ void RenderScene() {
 	//	(void*)0            // array buffer offset
 	//);
 	// Draw the triangle !
+	//glm::mat4 mvpMatrix = GetCurrentMVP(nEye); 
+	glm::mat4 mvpMatrix = MVPmatrix();
 	glUseProgram(g_renderFrameWindowProgramID);
+	glUniformMatrix4fv(m_frameWindowMatrixLocation, 1, GL_FALSE, &mvpMatrix[0][0]);
 	glBindVertexArray(g_frameWindowVAO);
 	glBindTexture(GL_TEXTURE_2D, m_frameTexture);
 	glDrawArrays(GL_TRIANGLES, 0, frameWindowVertCount); // Starting from vertex 0; 3 vertices total -> 1 triangle
 	glBindVertexArray(0);
 }
 
+void RenderSceneOnEye(vr::Hmd_Eye nEye) {
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	// 1rst attribute buffer : vertices
+	//glEnableVertexAttribArray(0);
+	//glBindBuffer(GL_ARRAY_BUFFER, frameWindowVertexbuffer);
+	//glVertexAttribPointer(
+	//	0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+	//	3,                  // size
+	//	GL_FLOAT,           // type
+	//	GL_FALSE,           // normalized?
+	//	0,                  // stride
+	//	(void*)0            // array buffer offset
+	//);
+	// Draw the triangle !
+	glm::mat4 mvpMatrix = GetCurrentMVP(nEye); //glm::mat4 mvpMatrix = MVPmatrix();
+	glUseProgram(g_renderFrameWindowProgramID);
+	glUniformMatrix4fv(m_frameWindowMatrixLocation, 1, GL_FALSE, &mvpMatrix[0][0]);
+	glBindVertexArray(g_frameWindowVAO);
+	glBindTexture(GL_TEXTURE_2D, m_frameTexture);
+	glDrawArrays(GL_TRIANGLES, 0, frameWindowVertCount); // Starting from vertex 0; 3 vertices total -> 1 triangle
+	glBindVertexArray(0);
+}
+
+void RenderStereoTargets()
+{
+	glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+	glEnable(GL_MULTISAMPLE);
+
+	// Left Eye
+	glBindFramebuffer(GL_FRAMEBUFFER, leftEyeDesc.m_nRenderFramebufferId);
+	glViewport(0, 0, m_nRenderWidth, m_nRenderHeight);
+	RenderSceneOnEye(vr::Eye_Left);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	glDisable(GL_MULTISAMPLE);
+
+	glBindFramebuffer(GL_READ_FRAMEBUFFER, leftEyeDesc.m_nRenderFramebufferId);
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, leftEyeDesc.m_nResolveFramebufferId);
+
+	glBlitFramebuffer(0, 0, m_nRenderWidth, m_nRenderHeight, 0, 0, m_nRenderWidth, m_nRenderHeight,
+		GL_COLOR_BUFFER_BIT,
+		GL_LINEAR);
+
+	glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+
+	glEnable(GL_MULTISAMPLE);
+
+	// Right Eye
+	glBindFramebuffer(GL_FRAMEBUFFER, rightEyeDesc.m_nRenderFramebufferId);
+	glViewport(0, 0, m_nRenderWidth, m_nRenderHeight);
+	RenderSceneOnEye(vr::Eye_Right);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	glDisable(GL_MULTISAMPLE);
+
+	glBindFramebuffer(GL_READ_FRAMEBUFFER, rightEyeDesc.m_nRenderFramebufferId);
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, rightEyeDesc.m_nResolveFramebufferId);
+
+	glBlitFramebuffer(0, 0, m_nRenderWidth, m_nRenderHeight, 0, 0, m_nRenderWidth, m_nRenderHeight,
+		GL_COLOR_BUFFER_BIT,
+		GL_LINEAR);
+
+	glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+}
+
 void onExit()
 {
-	g_pOpenVRGL->Release();
-	delete g_pOpenVRGL;
+	//g_pOpenVRGL->Release();
+	//delete g_pOpenVRGL;
 
-	//g_bRunning = false;
-	//g_threadLoadFrame.join();
 }
 
 //-------------------------------------------------------------------------------
@@ -185,7 +407,7 @@ bool CreateAllShaders() {
 
 		// Vertex Shader
 		"#version 410\n"
-		"//uniform mat4 matrix;\n"
+		"uniform mat4 matrix;\n"
 		"layout(location = 0) in vec4 position;\n"
 		"layout(location = 1) in vec2 v2UVcoordsIn;\n"
 		"//layout(location = 2) in vec3 v3NormalIn;\n"
@@ -193,8 +415,8 @@ bool CreateAllShaders() {
 		"void main()\n"
 		"{\n"
 		"	v2UVcoords = v2UVcoordsIn;\n"
-		"	//gl_Position = matrix * position;\n"
-		"	gl_Position = position;\n"
+		"	gl_Position = matrix * position;\n"
+		"	//gl_Position = position;\n"
 		"}\n",
 
 		// Fragment Shader
@@ -207,6 +429,7 @@ bool CreateAllShaders() {
 		"   outputColor = texture(mytexture, v2UVcoords);\n"
 		"}\n"
 	);
+	m_frameWindowMatrixLocation = glGetUniformLocation(g_renderFrameWindowProgramID, "matrix");
 	cout << "Shader Created!" << endl;
 	return g_renderFrameWindowProgramID != 0;
 }
@@ -231,9 +454,9 @@ bool SetupTexture() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 
-	//GLfloat fLargest;
-	//glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &fLargest);
-	//glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, fLargest);
+	GLfloat fLargest;
+	glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &fLargest);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, fLargest);
 
 	glBindTexture(GL_TEXTURE_2D, 0);
 
@@ -249,6 +472,79 @@ void UpdateFrameTexture() {
 }
 
 //-------------------------------------------------------------------------------
+// Purpose: Initialize
+//-------------------------------------------------------------------------------
+bool InitVRGL() {
+	// loading the  steamvr 
+	vr::EVRInitError eError = vr::VRInitError_None; // error message
+	m_pHMD = vr::VR_Init(&eError, vr::VRApplication_Scene);
+	if (eError != vr::VRInitError_None) { // error happens
+		m_pHMD = NULL;
+		return false;
+	}
+	m_pRenderModels = (vr::IVRRenderModels *)vr::VR_GetGenericInterface(vr::IVRRenderModels_Version, &eError);
+	if (!m_pRenderModels)
+	{
+		m_pHMD = NULL;
+		vr::VR_Shutdown();
+		return false;
+	}
+
+	// glew initialization
+	glewExperimental = GL_TRUE;
+	GLenum nGlewError = glewInit();
+	if (nGlewError != GLEW_OK)
+	{
+		printf("%s - Error initializing GLEW! %s\n", __FUNCTION__, glewGetErrorString(nGlewError));
+		return false;
+	}
+	glGetError(); // to clear the error caused deep in GLEW
+
+	// gl initialization
+	// opengl-related parameter
+	m_fNearClip = 0.1f;
+	m_fFarClip = 30.0f;
+	m_frameTexture = 0;
+	frameWindowVertCount = 0;
+	// setup list: texture, scene, camera, companion window( on PC monitor), device models.
+	SetupTexture();
+	SetupScene();
+	SetupCamera();
+	SetupStereoRenderTargets();
+	//SetupCompanionWindow();
+	//SetupRenderModels();	
+
+	// compositor initialization
+	vr::EVRInitError peError = vr::VRInitError_None;
+	if (!vr::VRCompositor())
+	{
+		printf("Compositor initialization failed. See log file for details\n");
+		return false;
+	}
+
+	return true;
+}
+
+void UpdateHMDPose() {
+	if (!m_pHMD)
+		return;
+
+	for (int nDevice = 0; nDevice < vr::k_unMaxTrackedDeviceCount; ++nDevice)
+	{
+		if (m_rTrackedDevicePose[nDevice].bPoseIsValid)
+		{
+			m_rmat4DevicePose[nDevice] = ConvertMat(m_rTrackedDevicePose[nDevice].mDeviceToAbsoluteTracking);
+		}
+	}
+
+	vr::VRCompositor()->WaitGetPoses(m_rTrackedDevicePose, vr::k_unMaxTrackedDeviceCount, NULL, 0);
+	if (m_rTrackedDevicePose[vr::k_unTrackedDeviceIndex_Hmd].bPoseIsValid)
+	{
+		m_mat4HMDPose = m_rmat4DevicePose[vr::k_unTrackedDeviceIndex_Hmd];
+	}
+}
+
+//-------------------------------------------------------------------------------
 // Purpose: glut callback function
 //-------------------------------------------------------------------------------
 void Display() {
@@ -256,13 +552,21 @@ void Display() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glEnable(GL_DEPTH_TEST);
 
-	RenderScene();
+	RenderStereoTargets();
+	//RenderScene();
+
+	vr::Texture_t leftEyeTexture = { (void*)(uintptr_t)leftEyeDesc.m_nResolveTextureId, vr::TextureType_OpenGL, vr::ColorSpace_Gamma };
+	vr::VRCompositor()->Submit(vr::Eye_Left, &leftEyeTexture);
+	vr::Texture_t rightEyeTexture = { (void*)(uintptr_t)rightEyeDesc.m_nResolveTextureId, vr::TextureType_OpenGL, vr::ColorSpace_Gamma };
+	vr::VRCompositor()->Submit(vr::Eye_Right, &rightEyeTexture);
 
 	glutSwapBuffers();
 	
 	tpNow = std::chrono::high_resolution_clock::now();
 	//std::cout << "FPS :" << 1000.0f / std::chrono::duration_cast<std::chrono::milliseconds>(tpNow - tpLast).count() << "\n";
 	tpLast = tpNow;
+
+	UpdateHMDPose();
 }
 
 void timer(int iVal)
@@ -330,8 +634,12 @@ int main(int argc, char* argv[]) {
 	CreateAllShaders();
 
 	// openvr setup
-	g_pOpenVRGL = new COpenVRGL();
-	g_pOpenVRGL->Initial(0.1f, 30.f);
+	//g_pOpenVRGL = new COpenVRGL();
+	//g_pOpenVRGL->Initial(0.1f, 30.f);
+
+	if (!InitVRGL()) {
+		return -1;
+	}
 
 	g_threadLoadFrame = std::thread([]() {
 		while (g_bRunning)
